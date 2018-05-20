1e1a698445952f8bc3cf7715afb440489e71347f
Khaled Ahmed <khaled.ahmed@tagipedia.com>
2018-05-20 13:45:09 +0200

    locate nearest places using gps

--- app/src/main/java/com/tagipedia/tmaps/MainActivity.java
+++ app/src/main/java/com/tagipedia/tmaps/MainActivity.java
@@ -1,18 +1,36 @@
 package com.tagipedia.tmaps;
 
+import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.GeolocationPermissions;
 import android.webkit.JavascriptInterface;
 import android.webkit.PermissionRequest;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -33,6 +51,11 @@ public class MainActivity extends Activity implements AdvancedWebView.Listener{
     WebView tmapWebView;
     private AdvancedWebView mWebView;
     private String mapId;
+    private String mGeolocationOrigin;
+    private GeolocationPermissions.Callback mGeolocationCallback;
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -82,7 +105,14 @@ public class MainActivity extends Activity implements AdvancedWebView.Listener{
         mWebView.reload();
         mWebView.loadUrl("file:///android_asset/tmapswww/index.html");
         mWebView.setWebChromeClient(new WebChromeClient() {
-
+            @Override
+            public void onGeolocationPermissionsShowPrompt(final String origin, final GeolocationPermissions.Callback callback) {
+                MainActivity.this.mGeolocationOrigin = origin;
+                MainActivity.this.mGeolocationCallback = callback;
+                if(checkLocationPermission()){
+                    displayLocationSettingsRequest();
+                }
+            }
             @Override
             public void onPermissionRequest(final PermissionRequest request) {
                 self.runOnUiThread(new Runnable() {
@@ -100,6 +130,75 @@ public class MainActivity extends Activity implements AdvancedWebView.Listener{
 
 
     }
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(this)
+                        .setTitle("NearBy")
+                        .setMessage("we cannot get nearby areas without Location access")
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void displayLocationSettingsRequest() {
+        GoogleApiClient googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API).build();
+        googleApiClient.connect();
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(10000 / 2);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        mGeolocationCallback.invoke(mGeolocationOrigin, true, false);
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            status.startResolutionForResult(MainActivity.this, REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        mGeolocationCallback.invoke(mGeolocationOrigin, false, false);
+                        break;
+                }
+            }
+        });
+    }
+
     public void dispatchMessage(final HashMap<String, Object> hashMap) {
         final JSONObject jsonObject = new JSONObject(hashMap);
         mWebView.post(new Runnable() {
@@ -158,6 +257,11 @@ public class MainActivity extends Activity implements AdvancedWebView.Listener{
                 }
             });
         } else if (message.get("type").equals("MAP_LOADED")){
+            dispatchMessage(new LinkedHashMap<String, Object>() {
+                {
+                    put("type", "ENABLE_GPS_BUTTON");
+                }
+            });
 
         } else if(message.get("type").equals("FEATURES_TAPPED")){
             dispatchMessage(new LinkedHashMap<String, Object>() {
@@ -168,8 +272,15 @@ public class MainActivity extends Activity implements AdvancedWebView.Listener{
             });
         } else if (message.get("type").equals("PROFILE_BUTTON_CLICKED")){
             new AlertDialog.Builder(this)
-                            .setTitle((String)message.get("type2"))
-                            .show();
+                    .setTitle((String)message.get("type2"))
+                    .show();
+        } else if (message.get("type").equals("CHECK_GPS_AVAILABILITY")){
+            dispatchMessage(new LinkedHashMap<String, Object>() {
+                {
+                    put("type", "START_UPDATING_LOCATION");
+                    put("isGpsActivated",true);
+                }
+            });
         }
     }
 
@@ -188,6 +299,47 @@ public class MainActivity extends Activity implements AdvancedWebView.Listener{
             return Utils.readFileFromAssets(this, "template.html");
         } catch (IOException e){
             return null;
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        displayLocationSettingsRequest();
+                    }
+                } else {
+                    // permission denied
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_CHECK_SETTINGS:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        if (mGeolocationCallback != null) {
+                            mGeolocationCallback.invoke(mGeolocationOrigin, true, false);
+                        }
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        if (mGeolocationCallback != null) {
+                            mGeolocationCallback.invoke(mGeolocationOrigin, false, false);
+                        }
+                        break;
+                }
+                break;
         }
     }
 
